1c8d2873a8bc31b666d5f2da4ccbc808c75d5f7b
Andrew Morrow <andrew.morrow@viam.com>
2025-02-04 10:58:24 -0500

    wip

--- esp-idf-components/blufi/include/blufi.h
+++ esp-idf-components/blufi/include/blufi.h
@@ -1,2 +1,8 @@
-void acmfunc(void);
 #include "esp_blufi_api.h"
+
+int blufi_aes_encrypt(uint8_t iv8, uint8_t *crypt_data, int crypt_len);
+int blufi_aes_decrypt(uint8_t iv8, uint8_t *crypt_data, int crypt_len);
+uint16_t blufi_crc_checksum(uint8_t iv8, uint8_t *data, int len);
+
+int blufi_security_init(void);
+void blufi_security_deinit(void);
